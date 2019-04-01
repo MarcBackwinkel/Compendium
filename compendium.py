10de628cfc94972704f50fd8ba5ed0ecf2f67ab3
marcBackwinkel <marc.backwinkel@gmail.com>
2019-04-01 11:19:12 +0000

    Chapter 1 and 2 done!

--- compendium.py
+++ compendium.py
@@ -1,1 +1,37 @@
-print("Hello World")
+### Chapter1. The way of the program
+#The first program
+print("Hello World")        #Hello World
+43+2                        #47
+84/2                        #42.0
+6^2                         #4     (XOR: 00000110 ^ 00000010 = 00000100)
+
+#Values and types
+type(2)                     #<class 'int'>
+type(42.0)                  #<class 'float'>
+type('text')                #<class 'str'>
+
+### Chapter2. Variables, expressions and statements
+#Keywords in Python
+False   class       finally     is          return
+None    continue    for         lambda      try
+True    def         from        nonlocal    while
+and     del         global      not         with
+as      elif        if          or          yield
+assert  else        import      pass
+break   except      in          raise
+
+#PEMDAS
+#Parentheses have highest precedence
+#Exponentiation: 2**3
+#Multiplication & Division have higher precedence than Addition & Subtraction
+
+#Strings
+"Hello" + " World"          #Hello World
+"Spam" * 3                  #SpamSpamSpam
+
+#Comments
+#Comments are most useful when they document non-obvious features of the code.
+#It is reasonable to assume that the reader can figure out what the code does;
+#it is more useful to explain why.
+
+### Chapter 3. Functions
